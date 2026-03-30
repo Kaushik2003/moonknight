21d29d7faee2f7da10dca883b493e70bbeebc985
rajashidattapy <rayan05rio@gmail.com>
2026-03-31 00:01:46 +0530

    added readme

--- frontend/old-frontend-components-and-hooks/components/useFileSystem.ts
+++
@@ -1,7 +0,0 @@
-import { useFileSystemContext, FileSystemItem } from "./FileSystemContext";
-
-export type { FileSystemItem };
-
-export function useFileSystem() {
-    return useFileSystemContext();
-}
